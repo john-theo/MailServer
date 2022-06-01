20e3c0f140a3b0ec406e4eacdb16b07fedf71ca7
John Dope <zhuangxh.cn@gmail.com>
2022-06-01 22:10:28 +0800

    ADD: template preview

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template as flask_render_template
 from src.render_template import render_template
 from src.mail import Attachment, Mail
 from werkzeug.exceptions import HTTPException
@@ -8,9 +8,6 @@ from src.flask_pyotp import PyOTP
 
 
 app = Flask(__name__)
-if not app.config['DEBUG']:
-    totp = PyOTP(app)
-    mail = Mail()
 
 
 @app.errorhandler(HTTPException)
@@ -43,10 +40,12 @@ def parse_request(request):
         kwargs = kwargs.to_dict()
         subject = kwargs.pop('subject')
     except KeyError:
-        raise MissingArguments('Both "receiver" and "subject" must be specified')
+        raise MissingArguments(
+            'Both "receiver" and "subject" must be specified')
     else:
         if not receivers or not subject:
-            raise MissingArguments('Both "receiver" and "subject" must be specified')
+            raise MissingArguments(
+                'Both "receiver" and "subject" must be specified')
     if 'template' in kwargs:
         template_name = kwargs.pop('template')
         html = render_template(template_name, **kwargs)
@@ -63,11 +62,21 @@ def index():
 
 
 @app.route('/debug', methods=['GET', 'POST'])
-def debug():
+@app.route('/preview', methods=['GET', 'POST'])
+def preview():
+    """Example:
+    http://127.0.0.1:5000/preview?subject=Please%20validate%20your%20email%20address&receiver=zhuangxh.cn@gmail.com&receiver=another@163.com&template=activation&username=John%20Dope&activation_link=https://zxh.cool&brand_name=Mail%20Server&contact_email=zhuangxh.cn@gmail.com&brand_logo_src=https://avatars.githubusercontent.com/u/36699957?v=4&brand_logo_height=30px&time_before=an%20hour&link_color=green&font_size=14px
+    """
     receivers, subject, html, attachments = parse_request(request)
-    return {
-        'receivers': receivers,
-        'subject': subject,
-        'html': html,
-        'attachments': [{"name": x.filename, "size": len(x.content)} for x in attachments]
-    }
+    return flask_render_template(
+        'preview.jinja', html=html, receivers=receivers, subject=subject,
+        attachments=[
+            {"name": x.filename, "size": len(x.content)} for x in attachments
+        ])
+
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    if not app.config['DEBUG']:
+        totp = PyOTP(app)
+        mail = Mail()
