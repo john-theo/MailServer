0615872cec8368df399acd33ddf0b436091c0b1c
John_Theo <zhuangxh.cn@gmail.com>
2022-06-01 19:36:22 +0800

    ADD: TOTP

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,11 @@
 from werkzeug.exceptions import HTTPException
 
 
+class InvalidTOTP(HTTPException):
+    code = 403
+    description = 'Invalid TOTP.'
+
+
 class TemplateNotFound(HTTPException):
     code = 404
     description = 'Template not found in "templates".'
